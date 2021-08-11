d127d7f4af0cbb06ef0c974537aba3de5fda65f9
Amrit Panthi <amritpanthi99@gmail.com>
2021-08-11 20:42:17 +0545

    Document fetched

--- src/pages/DocumentReview.jsx
+++ src/pages/DocumentReview.jsx
@@ -1,55 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Pages.css';
 
-const Customers = () => {
+const DocumentReview = () => {
+
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
         axios.get('http://localhost:90/driver-details/show')
-        .then(res =>{
-            console.log(res)
-            setPosts(res.data)
-        })
-        .catch(err=>{
-            console.log(err)
-        })  
+            .then(res => {
+                console.log(res)
+                setPosts(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
-    return(
+
+    return (
         <div>
             <ul>
                 {
-                    posts.map(post =>(
-                        <div className="col-3 border border-info">
+                    posts.map(post => (
+                        <table id='table'>
+                            <tr>
+                                <th>User Profile</th>
+                                <th>Phone Number</th>
+                                <th>Citizenship</th>
+                                <th>License</th>
+                                <th>Bluebook</th>
+                                <th>Remarks</th>
+                                <th>Edit</th>
 
-
-                                <img
-                                    className="img-itm"
-                                    src={`http://localhost:90/${post.image}`}
-                                    alt="document pic" />
-                                <div className="info-item">
-                                    <p className="item-info" id="name"> {post.Fullname}</p>
-                                    <p className="item-info" id="name"> {post.Phonenumber}</p>
-                                    <p className="item-info" id="name"> {post.Address}</p>
-                                    
-                                    
-                                </div><br></br>
-                                {/* <div className="itemaction">
-                                              
-                                              <button className="btn btn-danger" id="admbtndel" onClick={this.deleteProduct.bind(this, posts._id)}>  Delete </button>
-                                              <button className="btn btn-danger" id="admbtndel" onClick={this.deleteProduct.bind(this, posts._id)}>  Accept </button>
-                                        
-                                             
-                                            </div> */}
-
-                            </div>
+                            </tr>
+                            <tr>
+                                <td><img className="img-itm" src={`http://localhost:90/${post.image}`} alt="profile pic" /></td>
+                                <td><p className='item-info' id='name'>{post.Phonenumber}</p></td>
+                                <td><img className="img-itm-doc" src={`http://localhost:90/${post.citizenship}`} alt="citizenship pic" /></td>
+                                <td><img className="img-itm-doc" src={`http://localhost:90/${post.license}`} alt="license pic" /></td>
+                                <td><img className="img-itm-doc" src={`http://localhost:90/${post.bluebook}`} alt="bluebook pic" /></td>
+                            </tr>
+                        </table>
                     ))
                 }
             </ul>
         </div>
     )
-    
-   
 }
 
-export default Customers
+export default DocumentReview
